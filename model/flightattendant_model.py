0c799965e02491b97a26133adddbbe7b3dcb64e3
LogiFA <logi23@ru.is>
2023-12-04 13:23:36 +0000

    update flight attendants

--- model/flightattendant_model.py
+++ model/flightattendant_model.py
@@ -1,4 +1,7 @@
-class FlightAttendant:
-    def __init__(self, fligtAttendant_title) -> str:
+from model.employee_model import Employee
+
+class FlightAttendant(Employee):
+    def __init__(self, name, socialID, email, phonenumber, landline, fligtAttendant_title) -> str:
+        super().__init__(name, socialID, email, phonenumber, landline)
         self.flightAttendant_title = fligtAttendant_title
     
