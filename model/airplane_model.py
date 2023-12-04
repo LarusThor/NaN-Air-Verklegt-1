0bc3810bcb07b7b7cad3ab36b6f8f07ab98d1c70
LarusThor <142886101+LarusThor@users.noreply.github.com>
2023-12-04 13:59:49 +0000

    Added manufacturer to airplane

--- model/airplane_model.py
+++ model/airplane_model.py
@@ -1,8 +1,8 @@
 class Airplane:
-    def __init__(self,airplaneType,airplane,airplaneID,airplaneUsage,airplaneTime) -> str:
+    def __init__(self,airplaneType,airplane,airplaneID,airplaneUsage,airplaneTime, airplaneManufacturer) -> str:
         self.airplaneType = airplaneType
         self.airplane = airplane
         self.airplaneID = airplaneID
         self.airplaneUasge = airplaneUsage
         self.airplaneTime = airplaneTime
-        
+        self.airplaneManufacturer = airplaneManufacturer
