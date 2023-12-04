e41830390251fa1b08681d7688923e6e396f0bda
bjarki mar aronsson <bjarki1010@gmail.com>
2023-12-04 13:25:21 +0000

    scheduled complete

---
+++ model/schedule_model.py
@@ -0,0 +1,6 @@
+class Schedule:
+    def __init__(self, scheduledFlight, scheduledEmployee, scheduledDestination) -> str:
+        self.scheduledFlight = scheduledFlight
+        self.scheduledEmployee = scheduledEmployee
+        self.scheduledDestination = scheduledDestination
+        
