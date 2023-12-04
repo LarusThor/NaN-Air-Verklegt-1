730fe739e261bd55c9d6eac38e1a4a7d3abc3d62
LarusThor <142886101+LarusThor@users.noreply.github.com>
2023-12-04 15:54:00 +0000

    Update airplane_model.py

--- model/airplane_model.py
+++ model/airplane_model.py
@@ -1,26 +1,9 @@
 class Airplane:
-<<<<<<< Updated upstream
-    def __init__(self,airplaneType,airplaneInsignia,airplaneID,airplaneUsage,airplaneTime, airplaneManufacturer) -> str:
-        self.airplaneType = airplaneType
-        self.airplaneInsignia = airplaneInsignia
-=======
     def __init__(self, airplaneID, airplaneManufacturer, airplaneType, airplaneCapacity, airplaneInsignia ,airplaneUsage,airplaneTotalFlightTime) -> str:
->>>>>>> Stashed changes
         self.airplaneID = airplaneID
         self.airplaneManufacturer = airplaneManufacturer
-<<<<<<< Updated upstream
-
-
-# aircraft_type = model
-# aircraft = plane_insignia
-# aircraft_type = airplane_Type_id
-# passt flight = (aircraft_id)
-# past_flights = (arrival/departure)
-#aircraftType =  manafacturer
-=======
         self.airplaneType = airplaneType
         self.airplaneInsignia = airplaneInsignia
         self.airplaneCapacity = airplaneCapacity
         self.airplaneUsage = airplaneUsage
         self.airplaneTotalFlightTime = airplaneTotalFlightTime
->>>>>>> Stashed changes
