f77e8b8da23e5625c26685991e7c6a7af87f1fb1
LogiFA <logi23@ru.is>
2023-12-04 12:46:54 +0000

    Update employee_model.py

--- model/employee_model.py
+++ model/employee_model.py
@@ -2,11 +2,9 @@ class Employee:
     def __init__(self, name = "", socialID = "", email = "", phonenumber = 5812345, homephone = None):
         self.name = name
         self.socialID = socialID
+        self.email = email
+        self.phonenumber = phonenumber
+        self.homephone = homephone
+
+    def __str__(self) -> str:
         
-
-
-'''+ name: str
-# socialID: str
-+ email: str
-+ phoneNumber: str
-+ homePhone: int = default: None'''
