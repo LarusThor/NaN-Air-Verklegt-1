55b89ec6231dfea060ad640fa1e25ccf7a24666e
bjarki mar aronsson <bjarki1010@gmail.com>
2023-12-04 13:10:44 +0000

    Update airplane_model.py

--- model/airplane_model.py
+++ model/airplane_model.py
@@ -1,5 +1,5 @@
 class Airplane:
-    def __init__(self,airplaneType,airplane,airplaneID,airplaneUsage,airplaneTime) str:
+    def __init__(self,airplaneType,airplane,airplaneID,airplaneUsage,airplaneTime): str
         self.airplaneType = airplaneType
         self.airplane = airplane
         self.airplaneID = airplaneID
