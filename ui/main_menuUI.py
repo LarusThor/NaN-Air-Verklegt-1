f6d3a42e24bbcc7ea73f175c1fe985823ff37031
tinnamariat <tinnamariat@gmail.com>
2023-12-04 14:56:28 +0000

    menu-inn

--- ui/main_menuUI.py
+++ ui/main_menuUI.py
@@ -1,19 +1,39 @@
+import os
+
 LINE = "*"*30
+OPTIONS = ["1. Airplane", "2. Destinations", "3. Employees", "4. Schedule", "5. Voyages", "6. Flight information"]
 
 class MainMenu_UI:
     def __init__(self) -> None:
         pass
 
     def menu(self):
-        print("{}\n1. Airplane\n2. Destinations\n3. Employees\n4. Schedule\n5. VoyagesFlight information\n{}".format(LINE, LINE))
+    # Get terminal size
+        terminal_size = os.get_terminal_size()
+        terminal_width = terminal_size.columns
+        terminal_height = terminal_size.lines
+
+        # Calculate the number of empty lines above and below the menu
+        empty_lines_above = (terminal_height - 6) // 2
+        empty_lines_below = terminal_height - 6 - empty_lines_above - 2
+
+        # Print empty lines above the menu
+        print("\n" * empty_lines_above)
+
+        padding = (terminal_width) // 3
+        print(" " * padding + LINE)
+        # Print menu items centered in the terminal
+        for item in OPTIONS:
+            print(" " * padding + item)
+        print(" " * padding + LINE)
+
+        # Print empty lines below the menu
+        print("\n" * empty_lines_below)
 
     def input_prompt(self):
         while True:
             self.menu()
-            command = input("Enter your command: ")
-            command = command.lower()
+            action = input("Enter your action: ").lower()
 
 
 
-m = MainMenu_UI()
-m.input_prompt()
