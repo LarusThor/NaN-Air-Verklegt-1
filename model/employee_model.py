19e78f5ce2ecc042bd4b02579f2a1811302fe2cf
LogiFA <logi23@ru.is>
2023-12-04 12:36:35 +0000

    Update employee_model.py

--- model/employee_model.py
+++ model/employee_model.py
@@ -1,2 +1,12 @@
 class Employee:
-    pass
+    def __init__(self, name = "", socialID = "", email = "", phonenumber = 5812345, homephone = None):
+        self.name = name
+        self.socialID = socialID
+        
+
+
+'''+ name: str
+# socialID: str
++ email: str
++ phoneNumber: str
++ homePhone: int = default: None'''
