9711ea971fe233ec84d67b5f59378d2760627f10
tinnamariat <tinnamariat@gmail.com>
2023-12-04 13:48:47 +0000

    ui vinnsla

--- ui/main_menu.py
+++ ui/main_menu.py
@@ -1,6 +1,19 @@
+LINE = "*"*30
+
 class MainMenu_UI:
     def __init__(self) -> None:
         pass
 
     def menu(self):
-        pass
+        print("{}\n1. Airplane\n2. Destinations\n3. Employees\n4. Schedule\n5. VoyagesFlight information\n{}".format(LINE, LINE))
+
+    def input_prompt(self):
+        while True:
+            self.menu()
+            command = input("Enter your command: ")
+            command = command.lower()
+
+
+
+m = MainMenu_UI()
+m.input_prompt()
