ba2aad30a3a2fc71d96010ba86115808150f0510
LarusThor <142886101+LarusThor@users.noreply.github.com>
2023-12-04 13:17:02 +0000

    Pilot class

--- model/pilot_model.py
+++ model/pilot_model.py
@@ -1,7 +1,7 @@
 from model.employee_model import Employee
 
 class Pilot(Employee):
-    def __init__(self, name="", socialID="", email="", phonenumber=5812345, homephone=None, certification="", pilotTitle=""):
-        super().__init__(name, socialID, email, phonenumber, homephone, certification)
+    def __init__(self, name, socialID, email, phonenumber, landline, certification, pilotTitle) -> str:
+        super().__init__(name, socialID, email, phonenumber, landline)
         self.certification = certification
         self.pilotTitle = pilotTitle
