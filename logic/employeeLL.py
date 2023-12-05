9cf8ec7a3e78c9b7d94c703641b31874464d5fdb
evaxrut <136167068+evaxrut@users.noreply.github.com>
2023-12-05 10:10:03 +0000

    Update employeeLL.py

--- logic/employeeLL.py
+++ logic/employeeLL.py
@@ -2,9 +2,16 @@ from data.data_wrapper import DataWrapper
 
 class EmployeeLL():
     def __init__(self, employee_list:list) -> None:
-        for elem in employee_list:
-            name = key
-
+        #self.data_wrapper = data_connection
+        self.employee_list = employee_list
+        for key, value in employee_list:
+            self.name = key
+            self.social_id = value[0]
+            self.role = value[1]
+            self.rank = value[2]
+            self.licence = value[3]
+            self.address = value[4]
+            self.phone_nr = value[5]
 
     def get_employee_list(self):
         """ Returns a list of all employees within the system. """
