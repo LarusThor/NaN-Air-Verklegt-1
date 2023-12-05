34cdc582b80f271e75234a0f7b209f3a69f5d306
LogiFA <logi23@ru.is>
2023-12-05 09:51:42 +0000

    read destination into dicts

--- data/destinationIO.py
+++ data/destinationIO.py
@@ -1,7 +1,14 @@
+import csv
+
 class DestinationIO():
     def __init__(self):
         pass
 
     def read_destination(self):
-        
+        dest_list = []
+        with open('files\destinations.csv', newline='', encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                dest_list.append(row)
+        return dest_list
 
