d1988778858a3394a444bb86efc4109cbbf523aa
LarusThor <142886101+LarusThor@users.noreply.github.com>
2023-12-05 15:09:59 +0000

    Home address added to employee model

--- model/employee_model.py
+++ model/employee_model.py
@@ -1,13 +1,14 @@
 class Employee:
-    def __init__(self, name, socialID, email, phonenumber, landline) -> str:
+    def __init__(self, name, socialID, email, phonenumber, landline, home_address) -> str:
         self.name = name
         self.socialID = socialID
         self.email = email
         self.phonenumber = phonenumber
+        self.home_address = home_address
         self.landline = landline
-
 #name -> crew.csv -> name
 #socialID -> crew.csv -> nid
 #email -> crew.csv -> email
 #phonenumber -> crew.csv -> phone_nr
 #landline is optional
+#home_address -> crew.csv -> 
