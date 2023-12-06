33d0d8a3e9bfd61ba1137bf4ee5995f081959be5
LarusThor <142886101+LarusThor@users.noreply.github.com>
2023-12-06 17:01:48 +0000

    Aircraft data class updated

--- data/airplaneIO.py
+++ data/airplaneIO.py
@@ -11,23 +11,15 @@ class AirplaneIO:
             lines = f.readlines()
             for line in lines[1:]:
                 line = line.strip()
-                plane_insignia, plane_type_id, date_of_manufacture, last_maintenance = line.split(",")
-                aircraft = Airplane(plane_insignia, plane_type_id, date_of_manufacture, last_maintenance)
+                plane_insignia, plane_type_id, manufacturer, model, capacity = line.split(",")
+                aircraft = Airplane(plane_insignia, plane_type_id, manufacturer, model, capacity)
                 airplane_dict[plane_insignia] = (aircraft)
-    
-    def aircraft_types(self):
-        aircraft_types_dict = {}
-        with open("files/aircraft.csv", "r", newline='', encoding="utf-8") as f:
-            lines = f.readlines()
-            for line in lines[1:]:
-                line = line.strip()
-                plane_type_id, manufacturer, model, capacity = line.split(",")
-                aircraft_type = Airplane(plane_type_id, manufacturer, model, capacity)
-                aircraft_types_dict[plane_type_id] = (aircraft_type)        
-    
+
     def add_aircraft(self, aircraft):
-        with open('files/upcoming_flights.csv', 'a', newline='', encoding="utf-8") as csvfile:
-            fieldnames = ['flight_nr','dep_from','arr_at','departure','arrival','captain','copilot','fsm','fa1','fa2','fa3','fa4','fa5']
+        with open('files/aircraft.csv', 'a', newline='', encoding="utf-8") as csvfile:
+            fieldnames = ['plane_insignia','plane_type_id','manufacturer','model','capacity']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writerow({'plane_insignia': aircraft.plane_insignia, 'plane_type_id': aircraft.plane_type_id, 'manufacturer': aircraft.manufacturer, 'model': aircraft.model, 'capacity': aircraft.capacity})
 
             writer.writerow(aircraft)
