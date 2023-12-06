f1ed436cd67337c914d45e22fc7547cd12e4ffde
LarusThor <142886101+LarusThor@users.noreply.github.com>
2023-12-06 11:55:11 +0000

    Upcoming flights model & past flights

---
+++ model/past_flight_model.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+@dataclass
+class pastFlight:
+    flight_nr: str
+    dep_from: str
+    arr_at: str
+    departure: str
+    arrival: str
+    aircraft_id: str
+    captain: str
+    copilot: str
+    fsm: str
+    fa1:str
+    fa2: str
+    fa3: str
+    fa4: str
+    fa5: str
+
+#flight_nr,dep_from,arr_at,departure,arrival,aircraft_id,captain,copilot,fsm,fa1,fa2, seats_sold
