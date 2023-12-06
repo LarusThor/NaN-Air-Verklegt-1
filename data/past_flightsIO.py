ea8df09d093bfdfa1cfabed88adfb4316e86b850
LarusThor <142886101+LarusThor@users.noreply.github.com>
2023-12-06 14:22:48 +0000

    Upcoming flights & past flights IO & wrapper

--- data/past_flightsIO.py
+++ data/past_flightsIO.py
@@ -1,7 +1,7 @@
 import csv
 from model.past_flight_model import pastFlight
 
-class pastFlightsIO:
+class pastFlightIO:
 
     def __init__(self):
         pass
@@ -18,4 +18,11 @@ class pastFlightsIO:
 
         return past_flights_dict
     
+    def add_past_flight(self, past_flight):
+        with open('files/upcoming_flights.csv', 'a', newline='', encoding="utf-8") as csvfile:
+            fieldnames = ['flight_nr','dep_from','arr_at','departure','arrival','captain','copilot','fsm','fa1','fa2','fa3','fa4','fa5']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writerow(past_flight)
+
 #flight_nr,dep_from,arr_at,departure,arrival,aircraft_id,captain,copilot,fsm,fa1,fa2, fa3, fa4, fa5, seats_sold
