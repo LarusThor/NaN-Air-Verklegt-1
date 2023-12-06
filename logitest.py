bdbe478409c3ef6aace970f1d38c347bfc8fa2b3
LogiFA <logi23@ru.is>
2023-12-06 10:41:49 +0000

    read dest

--- logitest.py
+++ logitest.py
@@ -1,14 +1,17 @@
 from data.destinationIO import DestinationIO
 from model.destination_model import Destination
 
-dest = Destination(
+'''dest = Destination(
 destination_id = "test",
 destination = "test",
 airportName = "test",
 distanceFromIceland = "test",
 emergencyContact = "test",
-emergencyNumber = "test")
+emergencyNumber = "test")'''
+
 
 d = DestinationIO()
 
-d.add_destination(dest)
+dest_list = d.read_all_destinations()
+print(dest_list)
+'d.add_destination(dest)'
