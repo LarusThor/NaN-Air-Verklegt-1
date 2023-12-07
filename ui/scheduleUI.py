8e3a43216a5a4aaf0a4de946ae12d233f3816a13
evaxrut <136167068+evaxrut@users.noreply.github.com>
2023-12-07 13:38:38 +0000

    schedule fixed

--- ui/scheduleUI.py
+++ ui/scheduleUI.py
@@ -6,7 +6,7 @@ SCHEDULE_FOR_A_DAY_OPTIONS = ["1. See who was working", "2. See who was not work
 
 
 class ScheduleUI:
-    def init(self) -> None:
+    def __init__(self) -> None:
         self.menus = Menu()
         self.logic_wrapper = LogicWrapper()
 
@@ -20,7 +20,7 @@ class ScheduleUI:
         print("Input date as: day/month/year")
         date = input("Enter date: ")
         return date
-
+    
     def schedule_for_a_day_options(self) -> str:
         self.menus.display_options(SCHEDULE_FOR_A_DAY_OPTIONS)
         action = str(input("Enter your action: ").lower())
@@ -35,6 +35,8 @@ class ScheduleUI:
     def get_employee(self) -> str:
         employee = input("Enter the employees social ID: ")
         return employee
-
+    
     def get_schedule_for_employee(self, employee):
         pass
+
+        
