8febce7d4b62fd1c1b0e8ee16618770f192fc02b
evaxrut <136167068+evaxrut@users.noreply.github.com>
2023-12-11 12:57:54 +0000

    sma validation update

--- logic/validationLL.py
+++ logic/validationLL.py
@@ -1,6 +1,34 @@
 from data.data_wrapper import DataWrapper
+from datetime import datetime
 
 class ValidationLL():
     def __init__(self) -> None:
         self.data_wrapper = DataWrapper()
+        pass
+
+    def validate_name(self, name: str) -> bool:
+        pass
+
+    def validate_social_ID(self, socialID: str) -> bool:
+        pass
+
+    def validate_flight(self, flight: str) -> bool:
+        pass
+
+    def validate_address(self, address: str) -> bool:
+        pass
+
+    def validate_number(self, number: str) -> bool:
+        pass
+
+    def validate_email(self, email: str) -> bool:
+        pass
+
+    def validate_flight_nr(self, flight_nr: str) -> bool:
+        pass
+
+    def validate_voyage(self, voyage: str) -> bool:
+        pass
+
+    def date_validation(self, departure: datetime) -> bool:
         pass
