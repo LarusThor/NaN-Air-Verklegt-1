2e2494ceae1d243da7316b9ed4777fcf468ef549
LogiFA <logi23@ru.is>
2023-12-12 16:28:23 +0000

    UI tengja change destinations

--- logitest.py
+++ logitest.py
@@ -30,3 +30,20 @@ d = DestinationIO()
 dest_lidt = d.update_destination(dest)
 print(dest_lidt)
 
+'''        all_dests = self.logic_wrapper.destination_list()
+        counter = 0
+        for destination in all_dests:
+            if destination.destination_id == id:
+                break
+            else:
+                counter += 1
+        dest = all_dests[counter]
+        dest.destination_id = id
+        dest.destination = country
+        dest.emergency_contact_name = contact_name
+        dest.emergency_contact_number = contact_number
+        dest.airport_name = airport
+        dest.distance_from_iceland = distance_from_iceland
+        dest.estimated_flight_time = estimated_flight_time
+
+        self.logic_wrapper.update_destination_info(dest)'''
