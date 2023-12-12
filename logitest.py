0876601b5d40fc84e216cafffde4da46f258d985
LogiFA <logi23@ru.is>
2023-12-12 13:12:11 +0000

    update contact info

--- logitest.py
+++ logitest.py
@@ -2,14 +2,20 @@ from data.destinationIO import DestinationIO
 from model.destination_model import Destination
 from logic.destinationLL import DestinationLL
 
-dest = Destination(
-destination_id = "LWK",
-emergency_contact_name = "luigiboys",
-emergency_contact_number = "luigiboys")
+all_dests = DestinationIO().read_all_destinations()
+dest = all_dests[3]
+
+dest.emergency_contact_name = "luigiboys"
+dest.emergency_contact_number = "luigiboys"
+
+# dest = Destination(
+# destination_id = "LWK",
+# emergency_contact_name = "luigiboys",
+# emergency_contact_number = "luigiboys")
 
 
 d = DestinationIO()
 
-dest_lidt = d.update_contacts(dest)
+dest_lidt = d.update_destination(dest)
 print(dest_lidt)
 
