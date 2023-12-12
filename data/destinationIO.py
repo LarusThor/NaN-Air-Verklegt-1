960e96ece72249b62330387522418990b2531351
LarusThor <142886101+LarusThor@users.noreply.github.com>
2023-12-12 13:32:27 +0000

    Estimated flight time fix

--- data/destinationIO.py
+++ data/destinationIO.py
@@ -5,13 +5,23 @@ class DestinationIO():
     def __init__(self):
         pass
    
+    def read_destinations_info(self) -> dict:
+        destinations_overview = {}
+        with open("files/destinations.csv", "r") as f:
+            lines = f.readlines()
+            for line in lines[1:]:
+                line = line.strip()
+                id,destination,emergency_contact_name,emergency_contact_number,airport_name,distance_from_iceland, estimated_flight_time = line.split(",")
+                destination = Destination(id,destination,emergency_contact_name,emergency_contact_number,airport_name,distance_from_iceland, estimated_flight_time)
+                destinations_overview[id] = destination
+        return destinations_overview
 
     def read_all_destinations(self) -> list:
         dest_list = []
         with open('files/destinations.csv', 'r', newline='', encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                dest_list.append(Destination(row["id"], row["destination"], row["emergency_contact_name"], row["emergency_contact_number"], row["airport_name"], row["distance_from_iceland"]))
+                dest_list.append(Destination(row["id"], row["destination"], row["emergency_contact_name"], row["emergency_contact_number"], row["airport_name"], row["distance_from_iceland"], row['estimated_flight_time']))
             #id,destination,emergency_contact_name,emergency_contact_number,airport_name,distance_from_iceland, estimated_flight_time
             return dest_list
 
@@ -21,7 +31,7 @@ class DestinationIO():
             fieldnames = ['id','destination', 'emergency_contact_name', 'emergency_contact_number', 'airport_name', 'distance_from_iceland','estimated_flight_time']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-            writer.writerow({'id': destination.destination_id, 'destination': destination.destination, 'emergency_contact_name': destination.emergencyContact, 'emergency_contact_number': destination.emergencyNumber, 'airport_name': destination.airport_name, 'distance_from_iceland': destination.distanceFromIceland})
+            writer.writerow({'id': destination.destination_id, 'destination': destination.destination, 'emergency_contact_name': destination.emergencyContact, 'emergency_contact_number': destination.emergencyNumber, 'airport_name': destination.airport_name, 'distance_from_iceland': destination.distanceFromIceland, 'estimated_flight_time': destination.estimated_flight_time})
         
 
     def update_destination(self, updated_destination) -> None:
@@ -38,7 +48,7 @@ class DestinationIO():
             writer.writeheader()
             
             for destination in dest_list:
-                writer.writerow({'id': destination.destination_id, 'destination': destination.destination, 'emergency_contact_name': destination.emergency_contact_name, 'emergency_contact_number': destination.emergency_contact_number, 'airport_name': destination.airport_name, 'distance_from_iceland': destination.distance_from_iceland})
+                writer.writerow({'id': destination.destination_id, 'destination': destination.destination, 'emergency_contact_name': destination.emergency_contact_name, 'emergency_contact_number': destination.emergency_contact_number, 'airport_name': destination.airport_name, 'distance_from_iceland': destination.distance_from_iceland, 'estimated_flight_time': destination.estimated_flight_time})
 
     def update_contacts(self, updated_destination):
         dest_list = self.read_all_destinations()
@@ -50,19 +60,12 @@ class DestinationIO():
                 break
         
         with open('files/destinations.csv', 'w+', newline='', encoding="utf-8") as csvfile:
-            fieldnames = ['id','destination', 'emergency_contact_name', 'emergency_contact_number', 'airport_name', 'distance_from_iceland']
+            fieldnames = ['id','destination', 'emergency_contact_name', 'emergency_contact_number', 'airport_name', 'distance_from_iceland', 'estimated_flight_time']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             
             for destination in dest_list:
-                writer.writerow({'id': destination.destination_id, 'destination': destination.destination, 'emergency_contact_name': destination.emergency_contact_name, 'emergency_contact_number': destination.emergency_contact_number, 'airport_name': destination.airport_name, 'distance_from_iceland': destination.distance_from_iceland})
-        
-#destination_id -> destinations.csv -> id
-#destination -> destinations.csv -> destination
-#emergencyContact -> destinations.csv -> emergency_contact_name
-#emergencyNumber -> destinations.csv -> emergency_contact_number
-#airportName -> destinations.csv -> airport_name
-#distanceFromIceland -> destinations.csv -> distance_from_iceland
+                writer.writerow({'id': destination.destination_id, 'destination': destination.destination, 'emergency_contact_name': destination.emergency_contact_name, 'emergency_contact_number': destination.emergency_contact_number, 'airport_name': destination.airport_name, 'distance_from_iceland': destination.distance_from_iceland, 'estimated_flight_time': destination.estimated_flight_time})
 
 '''destination_id, destination, emergencyContact, emergencyNumber, 
                  airportName, distanceFromIceland'''
