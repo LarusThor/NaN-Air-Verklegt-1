31fa1abb70131e98b2d2981f16416468ea3bb518
evaxrut <136167068+evaxrut@users.noreply.github.com>
2023-12-13 15:41:31 +0000

    scheduleUI

--- eva_testskjal.py
+++ eva_testskjal.py
@@ -2,20 +2,23 @@ from ui.mainUI import Main
 from ui.scheduleUI import ScheduleUI
 from datetime import date,datetime
 from logic.logic_wrapper import LogicWrapper
+from ui.scheduleUI import ScheduleUI
 
 #kemur aldrei upp að þau séu að vinna í vikunni
 
-
 logic = LogicWrapper()
+start = datetime(2022, 11, 2)
+end = datetime(2023, 11, 2)
+employee = logic.employee_info("3009907461")
 
-employee = logic.employee_info("2410876598")
 
-print(logic.employee_schedule_by_week(employee.social_id, "2023", "44"))
+print(logic.total_hours_worked(employee, start, end))
+
+
 
 # ma = Main()
 # sc = ScheduleUI()
 
 
-# dt = date(2023, 11, 2)
 
 # print(sc.who_was_working(dt))
