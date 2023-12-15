b88924ba1515419f86141e992cfc06f26d05a26f
evaxrut <136167068+evaxrut@users.noreply.github.com>
2023-12-15 12:16:35 +0000

    Update flight_infoUI.py

--- ui/flight_infoUI.py
+++ ui/flight_infoUI.py
@@ -5,7 +5,7 @@ FLIGHT_STATUS_OPTIONS = ["1. Booking status for specific voyage", "2. Booking st
 
 class FlightInfoUI:
     def __init__(self) -> None:
-        """ TODO: add docstring """
+        """"Instantiate a ArplaneUI object."""
         self.logic_wrapper = LogicWrapper()
         self.menus = Menu()         
 
